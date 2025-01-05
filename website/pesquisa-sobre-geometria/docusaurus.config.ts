db4f357a6df7be96e5104e151c6b99f4c86e6db5
Eduardo dos Santos Leal <tcarvi@me.com>
2025-01-05 10:18:28 -0300

    Update

--- website/pesquisa-sobre-geometria/docusaurus.config.ts
+++ website/pesquisa-sobre-geometria/docusaurus.config.ts
@@ -66,6 +66,16 @@ const config: Config = {
     ],
   ],
 
+  stylesheets: [
+    {
+      href: 'https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css',
+      type: 'text/css',
+      integrity:
+        'sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM',
+      crossorigin: 'anonymous',
+    },
+  ],
+
   themeConfig: {
     // Replace with your project's social card
     image: 'img/docusaurus-social-card.jpg',
